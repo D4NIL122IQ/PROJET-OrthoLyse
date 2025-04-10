c2fdfcc3a4e2328f802c0cf9d8244c48f31b7d4e
Danil gdj <danilguidjou7@gmail.com>
2025-04-10 11:26:14 +0200

    controllers metrique fini

--- frontend/controllers/Result_controllers.py
+++ frontend/controllers/Result_controllers.py
@@ -7,46 +7,72 @@ import os
 
 
 # Ouvrir le fichier settings en mode lecture
-with open(os.path.abspath("../backend/suffixe.json"), 'r', encoding='utf-8') as fichier:
+with open(os.path.abspath("./settings.json"), 'r', encoding='utf-8') as fichier:
     # Charger le contenu du fichier JSON
     parametres = json.load(fichier)
 
 class ResultController:
     """
     Ce controller permet de faire le lien entre l'interface utilisateur et les analyses faite sur la transcription
+    toutes les methodes retourne un tableau [nbr_selon_methode , pourcentage]
+    le pourcentage est calculer comme suit : (nbr_resultat * 50) / (ratio * multiplicateur)
+    ===> multiplicateur = ratio * ( duree_audio / duree_ratio if(duree_audio >  duree_ratio) :  duree_ratio / duree_audio )
     """
     def __init__(self, transcrip="", file_path=""):
         self.resultat = Analyse_NLTK(text=transcrip)
         self.reultat_dic = parametres["ratio_metrique"]
-        self.audio_duree = file_size_sec(file_path)/60
+        duree_audio = file_size_sec(file_path)/60
+
+        if duree_audio >  self.reultat_dic["duree"]:
+            self.multiplicateur = duree_audio / self.reultat_dic["duree"];
+        else:
+            self.multiplicateur = self.reultat_dic["duree"] / duree_audio;
 
     def get_lemme(self):
         """Cette methode permet de renvoyer le nombre de lemme"""
-        return 0
-        #return  [self.resultat.calc_lemme()]
+        resultat = self.resultat.calc_lemme()
+        pourcentage = int((resultat * 50) / (self.reultat_dic["lemme"] * self.multiplicateur))
+        return  [resultat, pourcentage ]
 
     def get_word(self):
         """Cette methode permet de renvoyer le nombre de mot """
-        return 10
-        #return self.resultat.word_treatment()
+        resultat =len(self.resultat.word_treatment())
+        ratio = self.reultat_dic["nbrMot"]
+        print(ratio)
+        pourcentage = int((resultat* 50) // ( ratio * self.multiplicateur))
+        return [resultat, pourcentage]
 
     def get_dif_word(self):
         """Cette methode permet de renvoyer le nombre de mot different"""
-        return 30
-        #return self.resultat.nbr_unique_word()
+        resultat = self.resultat.nbr_unique_word()
+        pourcentage = int((resultat * 50) // (self.reultat_dic["motDif"] * self.multiplicateur))
+        return [resultat, pourcentage]
 
     def get_morpheme(self):
         """Cette methode permet de renvoyer le nombre de morpheme"""
-        return 40
-        #return self.resultat.morphem()
+        resultat = self.resultat.morphem()
+        pourcentage = int((resultat * 50) // (self.reultat_dic["morpheme"] * self.multiplicateur))
+        return [resultat, pourcentage]
 
     def get_enonce(self):
         """Cette methode permet de renvoyer le nombre d'enonce"""
-        return 60
-        #return self.resultat.sent_size()
+        resultat = self.resultat.sent_size()
+        pourcentage = int((resultat * 50) // (self.reultat_dic["nbrEnonce"] * self.multiplicateur))
+        return [resultat, pourcentage]
 
     def get_morpheme_enonce(self):
         """Cette methode permet de renvoyer le nombre de morpheme dans chaque enonces"""
-        return 10000
-        #return self.resultat.spacy_calc_morphem()
+        resultat = self.resultat.spacy_calc_morphem()
+        pourcentage = int((resultat * 50) // (self.reultat_dic["morphemeEnonce"] * self.multiplicateur))
+        return [resultat, pourcentage]
+
+    def export_pdf(self):
+        pass
+
+    def export_json(self):
+        pass
+
+    def export_docx(self):
+        pass
+
 
