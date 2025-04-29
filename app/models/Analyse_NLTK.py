a1a8aade8a16bedfb55d2e0d49eedfb142fad161
Danil gdj <danilguidjou7@gmail.com>
2025-04-29 12:55:03 +0200

    probleme de chemin reglé

--- app/models/Analyse_NLTK.py
+++ app/models/Analyse_NLTK.py
@@ -13,10 +13,10 @@ import json
 import os
 
 # Ouvrir le fichier JSON en mode lecture
-with open(os.path.abspath("./suffixe.json"), 'r', encoding='utf-8') as fichier:
+with open(os.path.abspath("./models/suffixe.json"), 'r', encoding='utf-8') as fichier:
     # Charger le contenu du fichier JSON
     suffixes = json.load(fichier)
-with open(os.path.abspath("./prefixe.json"), 'r', encoding='utf-8') as fichier:
+with open(os.path.abspath("./models/prefixe.json"), 'r', encoding='utf-8') as fichier:
     prefixes = json.load(fichier)
 
 
