3577d63d824796719eca6ee200fd816d40b1ace5
Danil gdj <danilguidjou7@gmail.com>
2025-05-07 23:27:52 +0200

    modif simple

--- app/controllers/Record_controllers.py
+++ app/controllers/Record_controllers.py
@@ -19,13 +19,13 @@ class RecordeController:
     def pause(self, widget):
         print("pause")
         self.recorder.volume_level.disconnect(widget.update_volume)
-        widget.set_pause_state(True)  # ← Stop animation + timer
+        widget.set_pause_state(True)  #  Stop animation + timer
         self.recorder.pause_recording()
 
     def retour_pause(self, widget):
         print("retour")
         self.recorder.volume_level.connect(widget.update_volume)
-        widget.set_pause_state(False)  # ← Reprend animation + timer
+        widget.set_pause_state(False)  #  Reprend animation + timer
         self.recorder.resume_recording()
 
     def get_final_file_path(self):
